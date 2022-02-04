79488b38cfe554da487a260a2d3a8201aaed3a95
dlatldhs <sionzz1713@gmail.com>
2022-02-04 21:41:43 +0900

    23251 번

---
+++ nojam_algorithm/23251.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int i,n,m23,sum,k;
+int main(){
+	//문제 23251 beakjoon
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		scanf("%d",&m23);
+		for(k=0;k<m23;k++){
+			sum += 23;
+		}
+		printf("%d\n",sum);
+		sum = 0;
+	}
+	return 0;
+}
